84b7ad5e50c006f76f55df266d88b449e0040f22
Brandon Guerra <brandong481@gmail.com>
2015-10-05 00:37:34 -0500

    working

--- index.js
+++ index.js
@@ -7,18 +7,10 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  // socket.on('message', function(msg){
-  //   io.emit('chat message', msg);
-  //   console.log(msg);
-  // });
-  socket.on('latData', function(lat){
-    io.emit('latitude: ', lat);
-    console.log(lat);
+  socket.on('latData', function(latData){
+    io.emit('latData', latData);
+    console.log(latData);
   });
-  // socket.on('lngData', function(lng){
-  //   io.emit('longitude: ', lng);
-  //   console.log(lng);
-  // });
 });
 
 server.listen(3000, function(){
